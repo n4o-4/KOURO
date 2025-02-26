d595435f3642179b2c0fa40e6a87a503bf79d0cb
takuma <k023g0015@g.neec.ac.jp>
2025-02-26 12:56:27 +0900

    playerかいた

---
+++ Player.cpp
@@ -0,0 +1,49 @@
+﻿#include "Player.h"
+#include <cmath>
+
+void Player::Initialize()
+{
+	// Object3d を初期化
+	object3d_->Initialize(Object3dCommon::GetInstance());
+	// モデルを設定
+	ModelManager::GetInstance()->LoadModel("Resources/player.obj");
+	object3d_->SetModel("Resources/player.obj");
+	// 初期位置を設定
+	objectTransform_->Initialize();
+}
+
+void Player::Update()
+{
+	// キー入力で移動
+	if (Input::GetInstance()->PushKey(DIK_W)) { Move({ 0.0f, 0.0f, 1.0f }); }  // 前進
+	if (Input::GetInstance()->PushKey(DIK_S)) { Move({ 0.0f, 0.0f, -1.0f }); } // 後退
+	if (Input::GetInstance()->PushKey(DIK_A)) { Move({ -1.0f, 0.0f, 0.0f }); } // 左移動
+	if (Input::GetInstance()->PushKey(DIK_D)) { Move({ 1.0f, 0.0f, 0.0f }); }  // 右移動
+	// 座標を適用
+	objectTransform_->transform.translate = position_;
+
+	objectTransform_->UpdateMatrix();
+	object3d_->Update();
+}
+
+void Player::Draw(ViewProjection viewProjection, DirectionalLight directionalLight, PointLight pointLight, SpotLight spotLight)
+{
+	// オブジェクトの描画
+	object3d_->Draw(*objectTransform_.get(), viewProjection, directionalLight, pointLight, spotLight);
+}
+
+void Player::Finalize()
+{
+
+}
+
+void Player::Move(Vector3 direction)
+{
+	// `MyMath.h` の `Normalize()` を使用して正規化
+	direction = Normalize(direction);
+
+	// 速度を適用して移動
+	position_.x += direction.x * speed_;
+	position_.y += direction.y * speed_;
+	position_.z += direction.z * speed_;
+}
--- application/scene/OkaScene.cpp
+++ application/scene/OkaScene.cpp
@@ -3,16 +3,37 @@
 void OkaScene::Initialize()
 {
 	BaseScene::Initialize();
+
+	directionalLight = std::make_unique<DirectionalLight>();
+	directionalLight->Initilaize();
+
+	pointLight = std::make_unique<PointLight>();
+	pointLight->Initilize();
+
+	spotLight = std::make_unique<SpotLight>();
+	spotLight->Initialize();
+
+	// プレイヤーを生成
+	player_ = std::make_unique<Player>();
+	player_->Initialize();
 }
 
 void OkaScene::Finalize()
 {
+	player_.reset();
 }
 
 void OkaScene::Update()
 {
+	// プレイヤーの更新
+	player_->Update();
 }
 
 void OkaScene::Draw()
 {
+	// プレイヤーの描画
+	player_->Draw(Camera::GetInstance()->GetViewProjection(),
+		*directionalLight.get(),
+		*pointLight.get(),
+		*spotLight.get());
 }
