1cd1e3fb22e072439796ff41a627959f5ebd5ae9
takuma <k023g0015@g.neec.ac.jp>
2025-02-26 14:48:27 +0900

    playerの描画&軽い動作

--- Player.cpp
+++ Player.cpp
@@ -18,13 +18,13 @@ void Player::Initialize()
 
 void Player::Update()
 {
-	//// キー入力で移動
-	//if (Input::GetInstance()->PushKey(DIK_W)) { Move({ 0.0f, 0.0f, 1.0f }); }  // 前進
-	//if (Input::GetInstance()->PushKey(DIK_S)) { Move({ 0.0f, 0.0f, -1.0f }); } // 後退
-	//if (Input::GetInstance()->PushKey(DIK_A)) { Move({ -1.0f, 0.0f, 0.0f }); } // 左移動
-	//if (Input::GetInstance()->PushKey(DIK_D)) { Move({ 1.0f, 0.0f, 0.0f }); }  // 右移動
-	//// 座標を適用
-	//objectTransform_->transform.translate = position_;
+	// キー入力で移動
+	if (Input::GetInstance()->PushKey(DIK_W)) { Move({ 0.0f, 0.0f, 1.0f }); }  // 前進
+	if (Input::GetInstance()->PushKey(DIK_S)) { Move({ 0.0f, 0.0f, -1.0f }); } // 後退
+	if (Input::GetInstance()->PushKey(DIK_A)) { Move({ -1.0f, 0.0f, 0.0f }); } // 左移動
+	if (Input::GetInstance()->PushKey(DIK_D)) { Move({ 1.0f, 0.0f, 0.0f }); }  // 右移動
+	// 座標を適用
+	objectTransform_->transform.translate = position_;
 
 	objectTransform_->UpdateMatrix();
 
