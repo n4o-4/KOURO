d373327ead92eaf17ae00108335562d894bb3dc5
n4o8 <146913762+n4o-4@users.noreply.github.com>
2025-02-28 22:11:35 +0900

    デバッグカメラ実装途中

---
+++ DebugCamera.cpp
@@ -0,0 +1,16 @@
+#include "DebugCamera.h"
+
+void DebugCamera::Initialize()
+{
+	BaseCamera::Initialize();
+}
+
+void DebugCamera::Update()
+{
+	// デバッグカメラ特有の処理
+
+
+
+	// 汎用変数更新
+	BaseCamera::Update();
+}
