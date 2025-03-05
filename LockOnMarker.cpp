b287171490c14910facff546746e575ebccf543c
PARK_YIJUN <flywalk@gmail.com>
2025-03-05 17:23:16 +0900

    a

--- LockOnMarker.cpp
+++ LockOnMarker.cpp
@@ -1,36 +1,110 @@
 #include "LockOnMarker.h"
 
-void LockOnMarker::Initialize() {
+void LockOnMarker::Initialize(size_t count) {
 
 	//Multi lock on
-	multiLockOn_ = std::make_unique<Object3d>();
-	multiLockOn_->Initialize(Object3dCommon::GetInstance());
-	ModelManager::GetInstance()->LoadModel("lockOn/Lock_on1.obj");
-	multiLockOn_->SetModel("lockOn/Lock_on1.obj");
 
-	multilockOnWorldTransform_ = std::make_unique<WorldTransform>();
-	multilockOnWorldTransform_->Initialize();
+	multiLockOns_.clear();
+	for (size_t i = 0; i < count; ++i) {
+		MultiLockOn lockOn;
 
-	isVisible_ = false;
+        // UR
+        lockOn.lockOnUR = std::make_unique<Object3d>();
+        lockOn.lockOnUR->Initialize(Object3dCommon::GetInstance());
+        ModelManager::GetInstance()->LoadModel("lockOn/Lock_onUR.obj");
+        lockOn.lockOnUR->SetModel("lockOn/Lock_onUR.obj");
+
+        lockOn.worldTransformUR = std::make_unique<WorldTransform>();
+        lockOn.worldTransformUR->Initialize();
+
+        // UL
+        lockOn.lockOnUL = std::make_unique<Object3d>();
+        lockOn.lockOnUL->Initialize(Object3dCommon::GetInstance());
+        ModelManager::GetInstance()->LoadModel("lockOn/Lock_onUL.obj");
+        lockOn.lockOnUL->SetModel("lockOn/Lock_onUL.obj");
+
+        lockOn.worldTransformUL = std::make_unique<WorldTransform>();
+        lockOn.worldTransformUL->Initialize();
+
+        // DR
+        lockOn.lockOnDR = std::make_unique<Object3d>();
+        lockOn.lockOnDR->Initialize(Object3dCommon::GetInstance());
+        ModelManager::GetInstance()->LoadModel("lockOn/Lock_onDR.obj");
+        lockOn.lockOnDR->SetModel("lockOn/Lock_onDR.obj");
+
+        lockOn.worldTransformDR = std::make_unique<WorldTransform>();
+        lockOn.worldTransformDR->Initialize();
+
+        // DL
+        lockOn.lockOnDL = std::make_unique<Object3d>();
+        lockOn.lockOnDL->Initialize(Object3dCommon::GetInstance());
+        ModelManager::GetInstance()->LoadModel("lockOn/Lock_onDL.obj");
+        lockOn.lockOnDL->SetModel("lockOn/Lock_onDL.obj");
+
+        lockOn.worldTransformDL = std::make_unique<WorldTransform>();
+        lockOn.worldTransformDL->Initialize();
+
+        lockOn.isVisible = false;
+        lockOn.rotation = 0.0f; 
+
+        multiLockOns_.push_back(std::move(lockOn));
+    }
+
+	
 
 }
 
-void LockOnMarker::Show() {
-	isVisible_ = true;
+void LockOnMarker::Show(size_t index) {
+    if (index < multiLockOns_.size()) {
+        multiLockOns_[index].isVisible = true;
+    }
 }
 
 void LockOnMarker::Hide() {
-	isVisible_ = false;
+    for (auto& lockOn : multiLockOns_) {
+        lockOn.isVisible = false;
+    }
 }
 
 void LockOnMarker::Update() {
-	multilockOnWorldTransform_->UpdateMatrix();
-	multiLockOn_->SetLocalMatrix(MakeIdentity4x4());
-	multiLockOn_->Update();
+    for (auto& lockOn : multiLockOns_) {
+        lockOn.rotation += 0.05f; 
+
+        lockOn.worldTransformUR->UpdateMatrix();
+        lockOn.worldTransformUL->UpdateMatrix();
+        lockOn.worldTransformDR->UpdateMatrix();
+        lockOn.worldTransformDL->UpdateMatrix();
+
+       /* lockOn.lockOnUR->SetLocalMatrix(RotateY(lockOn.rotation));
+        lockOn.lockOnUL->SetLocalMatrix(RotateY(lockOn.rotation));
+        lockOn.lockOnDR->SetLocalMatrix(RotateY(lockOn.rotation));
+        lockOn.lockOnDL->SetLocalMatrix(RotateY(lockOn.rotation));*/
+
+        lockOn.lockOnUR->Update();
+        lockOn.lockOnUL->Update();
+        lockOn.lockOnDR->Update();
+        lockOn.lockOnDL->Update();
+    }
 }
 
 void LockOnMarker::Draw(ViewProjection viewProjection, DirectionalLight directionalLight, PointLight pointLight, SpotLight spotLight) {
-	if (isVisible_) {
-	multiLockOn_->Draw(*multilockOnWorldTransform_.get(), viewProjection, directionalLight, pointLight, spotLight);
-	}
+    for (auto& lockOn : multiLockOns_) {
+        if (lockOn.isVisible) {
+            lockOn.lockOnUR->Draw(*lockOn.worldTransformUR, viewProjection, directionalLight, pointLight, spotLight);
+            lockOn.lockOnUL->Draw(*lockOn.worldTransformUL, viewProjection, directionalLight, pointLight, spotLight);
+            lockOn.lockOnDR->Draw(*lockOn.worldTransformDR, viewProjection, directionalLight, pointLight, spotLight);
+            lockOn.lockOnDL->Draw(*lockOn.worldTransformDL, viewProjection, directionalLight, pointLight, spotLight);
+        }
+    }
+}
+
+void LockOnMarker::SetTargetPosition(size_t index, const Vector3& position) {
+    if (index < multiLockOns_.size()) {
+        float offset = 1.0f;
+
+        multiLockOns_[index].worldTransformUR->transform.translate = { position.x + offset, position.y + offset, position.z };
+        multiLockOns_[index].worldTransformUL->transform.translate = { position.x - offset, position.y + offset, position.z };
+        multiLockOns_[index].worldTransformDR->transform.translate = { position.x + offset, position.y - offset, position.z };
+        multiLockOns_[index].worldTransformDL->transform.translate = { position.x - offset, position.y - offset, position.z };
+    }
 }
--- LockOnMarker.h
+++ LockOnMarker.h
@@ -5,24 +5,38 @@ class LockOnMarker {
 
 public:
 
-	void Initialize();
-	void Show();
+	void Initialize(size_t count);
+	void Show(size_t index);
 	void Hide();
 	void Update();
 	void Draw(ViewProjection viewProjection, DirectionalLight directionalLight, PointLight pointLight, SpotLight spotLight);
 
-	void SetTargetPosition(const Vector3& position) { multilockOnWorldTransform_->transform.translate = position; }
+	void SetTargetPosition(size_t index, const Vector3& position);
 
 
 private:
 
 
 	//Multi lock on
-	std::unique_ptr<Object3d> multiLockOn_ = nullptr;
-	std::unique_ptr<WorldTransform> multilockOnWorldTransform_ = nullptr;
-	bool isVisible_ = false;
+	/*std::vector<std::unique_ptr<Object3d>> multiLockOns_;
+	std::vector<std::unique_ptr<WorldTransform>> multiLockOnWorldTransforms_;
+	bool isVisible_ = false;*/
+	//
+	struct MultiLockOn {
+		std::unique_ptr<Object3d> lockOnUR;
+		std::unique_ptr<Object3d> lockOnUL;
+		std::unique_ptr<Object3d> lockOnDR;
+		std::unique_ptr<Object3d> lockOnDL;
 
+		std::unique_ptr<WorldTransform> worldTransformUR;
+		std::unique_ptr<WorldTransform> worldTransformUL;
+		std::unique_ptr<WorldTransform> worldTransformDR;
+		std::unique_ptr<WorldTransform> worldTransformDL;
 
+		bool isVisible = false;
+		float rotation = 0.0f; 
+	};
+	std::vector<MultiLockOn> multiLockOns_;
 
 };
 
