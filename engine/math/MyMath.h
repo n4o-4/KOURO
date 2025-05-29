a5f7dbb9ceda2fe2935827746a4a7d522c3101f8
n4o8 <146913762+n4o-4@users.noreply.github.com>
2025-05-29 11:18:19 +0900

    行列の逆行列計算の修正と更新
    `Inverse` 関数の `resultMatrix.m[1][3]` の計算式を修正し、`matrix.m[3][3]` から `matrix.m[2][3]` に変更しました。また、`resultMatrix.m[2][0]` の計算式を追加しました。これにより、行列の逆行列を計算する際の精度が向上します。

--- engine/math/MyMath.h
+++ engine/math/MyMath.h
@@ -358,13 +358,22 @@ static Matrix4x4 Inverse(Matrix4x4 matrix)
 			matrix.m[0][2] * matrix.m[1][0] * matrix.m[3][3] +
 			matrix.m[0][0] * matrix.m[1][3] * matrix.m[3][2]) / A;
 
-	resultMatrix.m[1][3] =
+	/*resultMatrix.m[1][3] =
 		(matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][3] +
 			matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][0] +
 			matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][2] -
 			matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][0] -
 			matrix.m[0][2] * matrix.m[1][0] * matrix.m[3][3] -
+			matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][2]) / A;*/
+
+	resultMatrix.m[1][3] =
+		(matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][3] +
+			matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][0] +
+			matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][2] -
+			matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][0] -
+			matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][3] - // 修正: m[3][3] → m[2][3]
 			matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][2]) / A;
+
 
 	resultMatrix.m[2][0] =
 		(matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][3] +
